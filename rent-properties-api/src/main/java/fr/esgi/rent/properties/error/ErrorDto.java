4ee27c9837451fddaf580c9a66c7659272b20ea5
Antsch <antoine97410@hotmail.fr>
2025-06-13 17:50:17 +0200

    refactor: Simplify ErrorDto using Java record structure

--- rent-properties-api/src/main/java/fr/esgi/rent/properties/error/ErrorDto.java
+++ rent-properties-api/src/main/java/fr/esgi/rent/properties/error/ErrorDto.java
@@ -1,13 +1,5 @@
 package fr.esgi.rent.properties.error;
 
-import lombok.Getter;
-
-@Getter
-public class ErrorDto {
-    private final String message;
-
-    public ErrorDto(String message) {
-        this.message = message;
-    }
+public record ErrorDto(String message) {
 
 }
